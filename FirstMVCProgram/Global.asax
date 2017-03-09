d72452cce9ad94556f5608a13d67e8730a180a34
Gurpreet <Gurpreet@VAIO>
2017-03-09 16:15:14 -0500

    Add project files.

---
+++ FirstMVCProgram/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstMVCProgram.MvcApplication" Language="C#" %>
---
+++ FirstMVCProgram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstMVCProgram.Startup))]
+namespace FirstMVCProgram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
